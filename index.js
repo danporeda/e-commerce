57820f6e093218c0d1dd5323b272dea757524dfa
Dan <danporeda@yahoo.com>
2024-01-17 22:48:13 -0500

    refactored GETs and POSTs from index.js to auth.js file with express.Router(). Moved GET response for signin and signup to files in views/admin/auth.

--- index.js
+++ index.js
@@ -1,101 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const userRepo = require('./repositories/users');
+const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({
-  keys: ['jdshjkdhfjkdfkskll']
-}))
+app.use(
+  cookieSession({
+    keys: ['jdshjkdhfjkdfkskll']
+  })
+);
+app.use(authRouter);
 
-app.get('/signup', (req, res) => {
-  res.send(`
-    <div>
-      Your User ID: ${req.session.userId}
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `);
-});
 
-app.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await userRepo.getOneBy({ email });
-
-  if (existingUser) {
-    return res.send('Email already in use');
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
-  }
-
-  const user = await userRepo.create({ email, password });
-
-  req.session.userId = user.id;
-
-  res.send('Account Created!!');
-});
-
-app.get('/signout', (req, res) => {
-  req.session = null;
-  res.send('You are logged out');
-});
-
-app.get('/signin', (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>Sign In</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await userRepo.getOneBy({ email });
-
-  if (!user) { 
-    return res.send('Account not found');
-  }
-
-  if (user.password !== password) {
-    return res.send('Invalid password');
-  }
-
-  req.session.userId = user.id;
-
-  return res.send('Logged in, Bucko!');
-})
 
 app.listen(3000, () => {
   console.log('Listening');
 });
-
-// const bodyParser = (req, res, next) => {
-//   if (req.method === 'POST') {
-//     req.on('data', data => {
-//       const parsed = data.toString('utf8').split('&');
-//       const formData = {};
-//       for (let pair of parsed) {
-//         const [key, value] = pair.split('=');
-//         formData[key] = value;
-//       }
-//       req.body = formData;
-//       next();
-//     });
-//   } else {
-//     next();
-//   }
-// };
---
+++ views/admin/auth/signup.js
@@ -0,0 +1,19 @@
+module.exports = ({ req }) => {
+  return `
+  <!DOCTYPE html>
+  <html>
+    <head></head>
+    <body>
+      <div>
+        Your User ID: ${req.session.userId}
+        <form method="POST">
+          <input name="email" placeholder="email" />
+          <input name="password" placeholder="password" />
+          <input name="passwordConfirmation" placeholder="password confirmation" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </body>
+  </html>
+  `;
+}
