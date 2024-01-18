b7ea5cd0ea8f3c864459f3f62662ae2bbd02f165
Dan <danporeda@yahoo.com>
2024-01-18 16:29:45 -0500

    refactors: installed express-validator, implemented on POST /signup. logic exported from validators.js in same directory

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require ('../../views/admin/auth/signin');
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
 
 const router = express.Router();
 
@@ -9,25 +12,25 @@ router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
+router.post(
+  '/signup', 
+  [
+    requireEmail,
+    requirePassword,
+    requirePasswordConfirmation
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
 
-  const existingUser = await usersRepo.getOneBy({ email });
+    const { email, password } = req.body;
+    const user = await usersRepo.create({ email, password });
 
-  if (existingUser) {
-    return res.send('Email already in use');
+    req.session.userId = user.id;
+
+    res.send('Account Created!!');
   }
-
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
-  }
-
-  const user = await usersRepo.create({ email, password });
-
-  req.session.userId = user.id;
-
-  res.send('Account Created!!');
-});
+);
 
 router.get('/signout', (req, res) => {
   req.session = null;
