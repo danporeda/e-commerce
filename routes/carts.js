d3028524318fcc652d8fad86f6e3a11303889197
Dan <danporeda@yahoo.com>
2024-01-29 13:49:56 -0500

    added GET request /cart, and template in views/carts/show

--- routes/carts.js
+++ routes/carts.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts')
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
 router.post('/cart/products', async (req, res) => {
-  // Figure out the cart
   let cart;
   if (!req.session.cartId) {
-    // We don't have a cart, we need to create one,
-    // and store the cart id on the req.session.cartId
-    // property
     cart = await cartsRepo.create({ items: [] });
     req.session.cartId = cart.id;
   } else {
-    // we have a cart, get it from repository
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
@@ -27,6 +24,22 @@ router.post('/cart/products', async (req, res) => {
     items: cart.items
   });
   res.send('Product added to cart');
+});
+
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect('/');
+  } 
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
 })
 
 module.exports = router;
